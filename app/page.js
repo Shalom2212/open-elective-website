54fb18e61608b60fcdb034129131525cb41f9486
Shalom2212 <shalom222002@gmail.com>
2024-04-28 08:15:52 +0530

    added console fetch data subjects

--- app/page.js
+++ app/page.js
@@ -61,6 +61,7 @@ export default function Home() {
         //     timestamp: Date.now(),
         //   },
         // });
+        console.log("fetch data subjects");
         const response = await fetch("/api/subjects", {
           cache: "no-store",
         });
